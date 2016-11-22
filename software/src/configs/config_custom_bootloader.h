0d954686a9afd105c8dd1d9fbd0268140f35b837
Olaf Lüke <olaf@tinkerforge.com>
2016-11-22 18:19:43 +0100

    Use external oscillator

--- software/src/configs/config_custom_bootloader.h
+++ software/src/configs/config_custom_bootloader.h
@@ -36,6 +36,11 @@
 #define BOOTLOADER_FLASH_SIZE          (64*1024)
 
 
+// --- Oscilater defines ---
+#define OSCHP_FREQUENCY 16000000
+#define DCLK_CLOCK_SRC  1
+
+
 // --- SPITFP ---
 #define SPITFP_USIC_CHANNEL         USIC0_CH1
 #define SPITFP_USIC                 XMC_SPI0_CH1
