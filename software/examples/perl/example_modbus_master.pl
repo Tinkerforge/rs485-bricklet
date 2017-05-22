4f12fc7cd746c4eed5a97032c9c5601370358193
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-05-22 15:06:56 +0200

    Add Modbus master and slave Perl examples

---
+++ software/examples/perl/example_modbus_master.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletRS485;
+use threads::shared;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'XYZ'; # Change XYZ to the UID of your RS485 Bricklet
+
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $rs485 = Tinkerforge::BrickletRS485->new(&UID, $ipcon); # Create device object
+
+my $expected_request_id :shared = 0;
+
+# Callback subroutine for write single register response callback
+sub cb_modbus_master_write_single_register_response
+{
+  my ($request_id, $exception_code) = @_;
+
+  if($request_id != $expected_request_id)
+  {
+    print "Unexpected request ID\n";
+
+    return;
+  }
+
+  print "Request ID = " . $request_id . "\n";
+  print "Exception Code = " . $exception_code . "\n";
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connecte
+
+# Set operating mode of the Bricklet
+$rs485->set_mode($rs485->MODE_MODBUS_MASTER_RTU);
+
+# Modbus specific configuration
+#
+# Slave address = 1 (Unused in master mode)
+# Request timeout = 1000ms (Unused in slave mode)
+$rs485->set_modbus_configuration(1, 1000);
+
+# Register write single register response callback
+$rs485->register_callback($rs485->CALLBACK_MODBUS_MASTER_WRITE_SINGLE_REGISTER_RESPONSE,
+                          'cb_modbus_master_write_single_register_response');
+
+# Request single register write
+$expected_request_id = $rs485->modbus_master_write_single_register(1, 42, 65535);
+
+print "Press key to exit\n";
+<STDIN>;
+$ipcon->disconnect();
---
+++ software/examples/perl/example_modbus_slave.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use Tinkerforge::IPConnection;
+use Tinkerforge::BrickletRS485;
+use threads::shared;
+
+use constant HOST => 'localhost';
+use constant PORT => 4223;
+use constant UID => 'XYZ'; # Change XYZ to the UID of your RS485 Bricklet
+
+my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
+my $rs485 :shared = Tinkerforge::BrickletRS485->new(&UID, $ipcon); # Create device object
+
+# Callback subroutine for write single register request callback
+sub cb_modbus_slave_write_single_register_request
+{
+  my ($request_id, $register_address, $register_value) = @_;
+
+  print "Request ID = " . $request_id . "\n";
+  print "Register Address = " . $register_address . "\n";
+  print "Register Value = " . $register_value . "\n";
+
+  # Here we assume valid writable register address is 42
+  if($register_address != 42)
+  {
+      print "Requested invalid register address\n";
+      $rs485->modbus_slave_report_exception($request_id, $rs485->EXCEPTION_CODE_ILLEGAL_DATA_ADDRESS);
+  }
+  else
+  {
+      print "Request OK";
+      $rs485->modbus_slave_answer_write_single_register_request($request_id);
+  }
+}
+
+$ipcon->connect(&HOST, &PORT); # Connect to brickd
+# Don't use device before ipcon is connecte
+
+# Set operating mode of the Bricklet
+$rs485->set_mode($rs485->MODE_MODBUS_SLAVE_RTU);
+
+# Modbus specific configuration
+#
+# Slave address = 1 (Unused in master mode)
+# Request timeout = 1000ms (Unused in slave mode)
+$rs485->set_modbus_configuration(1, 1000);
+
+# Register write single register request callback
+$rs485->register_callback($rs485->CALLBACK_MODBUS_SLAVE_WRITE_SINGLE_REGISTER_REQUEST,
+                          'cb_modbus_slave_write_single_register_request');
+
+print "Press key to exit\n";
+<STDIN>;
+$ipcon->disconnect();
