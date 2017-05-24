7cc24fb9f32641852acce82f8d4c549640fc3a3d
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-05-24 13:42:04 +0200

    Add shell loopback example

---
+++ software/examples/shell/example-loopback.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Connects to localhost:4223 by default, use --host and --port to change this
+
+# For this example connect the RX+/- pins to TX+/- pins on the same bricklet to
+# receive the sent message
+
+uid=MST # Change XYZ to the UID of your RS485 Bricklet
+
+# Handle incoming read callback
+tinkerforge dispatch rs485-bricklet $uid read &
+
+# Enable read callback
+tinkerforge call rs485-bricklet $uid enable-read-callback
+
+# Write "test" string
+tinkerforge call rs485-bricklet $uid write t,e,s,t,.. 4
+
+echo "Press key to exit"; read dummy
+
+kill -- -$$ # Stop callback dispatch in background
