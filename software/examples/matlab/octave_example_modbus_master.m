d737d437aef46a6c4659732f6c033dc1f872bdc6
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-05-24 14:37:04 +0200

    Add Modbus master and slave Octave examples

---
+++ software/examples/matlab/octave_example_modbus_master.m
@@ -0,0 +1,47 @@
+function octave_example_modbus_master()
+    more off;
+    global expected_request_id;
+
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "XYZ"; % Change XYZ to the UID of your RS485 Bricklet
+
+    % For newer version of Octave instead of "java_new" use "javaObject"
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    rs485 = java_new("com.tinkerforge.BrickletRS485", UID, ipcon); % Create device object;
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Set operating mode of the Bricklet
+    rs485.setMode(rs485.MODE_MODBUS_MASTER_RTU);
+
+    % Modbus specific configuration
+    %
+    % Slave address = 1 (Unused in master mode)
+    % Request timeout = 1000ms (Unused in slave mode)
+    rs485.setModbusConfiguration(1, 1000);
+
+    % Register write single register response callback
+    rs485.addModbusMasterWriteSingleRegisterResponseCallback(@cb_modbus_master_write_single_register_response);
+
+    % Request single register write
+    expected_request_id = rs485.modbusMasterWriteSingleRegister(1, 42, 65535);
+
+    input("Press key to exit\n", "s");
+    ipcon.disconnect();
+end
+
+% Callback function for write single register response callback
+function cb_modbus_master_write_single_register_response(e)
+    global expected_request_id;
+
+    if e.requestID ~= expected_request_id
+        fprintf("Unexpected request ID\n");
+
+        return;
+    end
+
+    fprintf("Request ID = %g\n", e.requestID);
+    fprintf("Exception Code = %g\n", e.exceptionCode);
+end
---
+++ software/examples/matlab/octave_example_modbus_slave.m
@@ -0,0 +1,48 @@
+function octave_example_modbus_slave()
+    more off;
+    global rs485;
+
+    HOST = "localhost";
+    PORT = 4223;
+    UID = "XYZ"; % Change XYZ to the UID of your RS485 Bricklet
+
+    % For newer version of Octave instead of "java_new" use "javaObject"
+    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
+    rs485 = java_new("com.tinkerforge.BrickletRS485", UID, ipcon); % Create device object;
+
+    ipcon.connect(HOST, PORT); % Connect to brickd
+    % Don't use device before ipcon is connected
+
+    % Set operating mode of the Bricklet
+    rs485.setMode(rs485.MODE_MODBUS_SLAVE_RTU);
+
+    % Modbus specific configuration
+    %
+    % Slave address = 1 (Unused in master mode)
+    % Request timeout = 1000ms (Unused in slave mode)
+    rs485.setModbusConfiguration(1, 1000);
+
+    % Register write single register request callback
+    rs485.addModbusSlaveWriteSingleRegisterRequestCallback(@cb_modbus_slave_write_single_register_request);
+
+    input("Press key to exit\n", "s");
+    ipcon.disconnect();
+end
+
+% Callback function for write single register request callback
+function cb_modbus_slave_write_single_register_request(e)
+    global rs485;
+
+    fprintf("Request ID = %g\n", e.requestID);
+    fprintf("Register Address = %g\n", e.registerAddress);
+    fprintf("Register Value = %g\n", e.registerValue);
+
+    % Here we assume valid writable register address is 42
+    if e.registerAddress ~= 42
+        fprintf("Requested invalid register address\n");
+        rs485.modbusSlaveReportException(e.requestID, rs485.EXCEPTION_CODE_ILLEGAL_DATA_ADDRESS);
+    else
+        fprintf("Request OK\n");
+        rs485.modbusSlaveAnswerWriteSingleRegisterRequest(e.requestID);
+    end
+end
