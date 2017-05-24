2aec56bcc83e577af33a9c209bc96696d6c5edaf
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-05-24 16:57:13 +0200

    Add Delphi/Lazarus loopback example

---
+++ software/examples/delphi/ExampleLoopback.pas
@@ -0,0 +1,72 @@
+program ExampleLoopback;
+
+{$ifdef MSWINDOWS}{$apptype CONSOLE}{$endif}
+{$ifdef FPC}{$mode OBJFPC}{$H+}{$endif}
+
+uses
+  SysUtils, IPConnection, BrickletRS485;
+
+  {
+    For this example connect the RX+/- pins to TX+/- pins on the same bricklet
+    and configure the Bricklet to be in full-duplex mode
+  }
+
+type
+  TExampleLoopback = class
+  private
+    ipcon: TIPConnection;
+    rs485: TBrickletRS485;
+  public
+    procedure ReadCB(sender: TBrickletRS485; const message: array of byte);
+    procedure Execute;
+  end;
+
+const
+  HOST = 'localhost';
+  PORT = 4223;
+  UID = 'XYZ'; { Change XYZ to the UID of your RS485 Bricklet }
+
+var
+  e: TExampleLoopback;
+  bufferString: string;
+  bufferBytes: array of byte;
+
+{ Callback function for read callback }
+procedure TExampleLoopback.ReadCB(sender: TBrickletRS485; const message: array of byte);
+begin
+  WriteLn(Format('Message (Length: %d): "%s"', [length(message), StrPas(message)]);
+end;
+
+procedure TExampleLoopback.Execute;
+begin
+  { Create IP connection }
+  ipcon := TIPConnection.Create;
+
+  { Create device object }
+  rs485 := TBrickletRS485.Create(UID, ipcon);
+
+  { Connect to brickd }
+  ipcon.Connect(HOST, PORT);
+  { Don't use device before ipcon is connected }
+
+  { Enable read callback }
+  rs485.EnableReadCallback();
+
+  { Register read callback to function ReadCB }
+  rs485.OnRead := {$ifdef FPC}@{$endif}ReadCB;
+
+  { Write "test" string}
+  bufferString := "test";
+  Move(bufferString[1], bufferBytes^, Length(bufferString));
+  rs485.Write(bufferBytes);
+
+  WriteLn('Press key to exit');
+  ReadLn;
+  ipcon.Destroy; { Calls ipcon.Disconnect internally }
+end;
+
+begin
+  e := TExampleLoopback.Create;
+  e.Execute;
+  e.Destroy;
+end.
