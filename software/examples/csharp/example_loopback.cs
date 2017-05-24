508c93d7727f6aa62e98fd4d10b37087580d0ec1
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-05-24 16:27:09 +0200

    Add C# loopback example

---
+++ software/examples/csharp/example_loopback.cs
@@ -0,0 +1,41 @@
+using System;
+using Tinkerforge;
+
+// For this example connect the RX+/- pins to TX+/- pins on the same bricklet
+// and configure the Bricklet to be in full-duplex mode
+
+class ExampleLoopback
+{
+  private static string HOST = "localhost";
+  private static int PORT = 4223;
+  private static string UID = "XYZ"; // Change XYZ to the UID of your RS485 Bricklet
+
+  // Callback function for read callback
+  static void ReadCB(BrickletRS485 sender, char[] message)
+  {
+    Console.WriteLine("Message (Length: {0}): \"{1}\"\n", message.length, new string(message));
+  }
+
+  static void Main()
+  {
+    IPConnection ipcon = new IPConnection(); // Create IP connection
+    BrickletRS485 rs485 = new BrickletRS485(UID, ipcon); // Create device object
+
+    ipcon.Connect(HOST, PORT); // Connect to brickd
+    // Don't use device before ipcon is connected
+
+    // Register read callback to function ReadCB
+    rs485.ReadCallback += ReadCB;
+
+    // Enable read callback
+    rs485.EnableReadCallback();
+
+    // Write "test" string
+    string buffer = "test";
+    rs485.write(buffer.ToCharArray());
+
+    Console.WriteLine("Press enter to exit");
+    Console.ReadLine();
+    ipcon.Disconnect();
+  }
+}
