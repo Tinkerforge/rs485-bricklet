40f42aa138e8e3e8889f352e18c624402952777f
Matthias Bolte <matthias@tinkerforge.com>
2017-07-17 17:13:09 +0200

    shell: Fix slave address in example

--- software/examples/shell/example-modbus-master.sh
+++ software/examples/shell/example-modbus-master.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Connects to localhost:4223 by default, use --host and --port to change this
 
-
 uid=XYZ # Change XYZ to the UID of your RS485 Bricklet
+
 export _tmp=$(mktemp)
 
 # Register write single register response callback
-tinkerforge dispatch rs485-bricklet $uid modbus-master-write-single-register-response \
+tinkerforge dispatch rs485-bricklet $uid modbus-master-write-single-register-response\
   --execute "export _expected-request-id=\$(cat \$_tmp)
 
              if [ {request-id} != \"\$_expected-request-id\" ]; then
@@ -25,8 +25,9 @@ tinkerforge call rs485-bricklet $uid set-mode mode-modbus-master-rtu
 # - slave address = 1 (unused in master mode)
 # - master request timeout = 1000ms
 tinkerforge call rs485-bricklet $uid set-modbus-configuration 1 1000
+
 # Request single register write
-tinkerforge call rs485-bricklet $uid modbus-master-write-single-register 1 42 65535 \
+tinkerforge call rs485-bricklet $uid modbus-master-write-single-register 17 42 65535\
   --execute "echo {request-id} > ${_tmp}"
 
 echo "Press key to exit"; read dummy
